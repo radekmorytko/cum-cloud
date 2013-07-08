24bb20b202eeaaa1fcb46ddca119e0142ac2be6d
Dariusz Chrząścik <dariusz@chrzascik.com>
2013-07-08 22:33:29 +0200

    Added separate task for package generation

--- cum-oneapps-3.8.3/rakefile.rb
+++ cum-oneapps-3.8.3/rakefile.rb
@@ -3,11 +3,14 @@ require 'rake/testtask'
 
 task :default => :deploy
 
-task :deploy, :host, :package do |t, args|
+task :gen_package do
+  puts %x{cd src && ./gen_package.sh}
+end
+
+task :deploy, [:host, :package] => [:gen_package] do |t, args|
   args.with_defaults(:host => :'one', :package => 'oneapps_3.8.3.deb')
   user = 'root'
 
-  puts %x{cd src && ./gen_package.sh}
   puts %x{scp src/#{args[:package]} root@#{args[:host]}:~}
   puts %x{ssh root@#{args[:host]} dpkg -i /root/#{args[:package]}}
 end
