37bb703bb53fef67cc27b4d7f7e4517c75a22e93
Dariusz Chrząścik <dariusz@chrzascik.com>
2013-07-10 18:46:34 +0200

    Updated image boostraping procces

--- examples/testbed/cookbooks/opennebula/recipes/image.rb
+++ examples/testbed/cookbooks/opennebula/recipes/image.rb
@@ -8,18 +8,34 @@ opennebula_pkg_name = 'app-context_3.8.2.deb'
 opennebula_pkg_dst = "/tmp/#{opennebula_pkg_name}"
 
 cookbook_file opennebula_pkg_dst do
-	source opennebula_pkg_name
-	mode "0444"
+  source opennebula_pkg_name
+  mode "0444"
 end
 
 package opennebula_pkg_name do
-	provider Chef::Provider::Package::Dpkg
-	source opennebula_pkg_dst
-	action :install
+  provider Chef::Provider::Package::Dpkg
+  source opennebula_pkg_dst
+  action :install
+end
+
+# vm-coordinator
+# oneapps
+vm_coordinator_pkg_name = 'vm_coordinator_3.8.3.deb'
+vm_coordinator_pkg_dst = "/tmp/#{opennebula_pkg_name}"
+
+cookbook_file vm_coordinator_pkg_dst do
+  source vm_coordinator_pkg_name
+  mode "0444"
+end
+
+package vm_coordinator_pkg_name do
+  provider Chef::Provider::Package::Dpkg
+  source vm_coordinator_pkg_dst
+  action :install
 end
 
 # cum-oneapps
-%w(json sinatra rest-client redis).each do |gem|
+%w(json sinatra rest-client).each do |gem|
   gem_package(gem) do
     gem_binary "gem"
   end
@@ -29,7 +45,3 @@ oneapps_path = '/usr/lib/one/ruby/oneapps'
 directory oneapps_path do
    recursive true
 end
-
-# symlink
-execute "rm -rf /etc/one-context.d/99-one-chef"
-execute "ln -s #{oneapps_path}/vm_coordinator/99-one-chef /etc/one-context.d/99-one-chef"
