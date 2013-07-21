df28b05d1437b36959f6179bc6d6e7cf29779ca4
Dariusz Chrząścik <dariusz@chrzascik.com>
2013-07-21 11:30:24 +0200

    [#17] vm_coordinator sends feedback to auto-scaling-server

--- baseimage/src/vm_coordinator/bin/vm_coordinator.rb
+++ baseimage/src/vm_coordinator/bin/vm_coordinator.rb
@@ -2,6 +2,9 @@
 
 $LOAD_PATH.unshift(File.dirname(File.expand_path('..', __FILE__)))
 
+require 'rubygems'
+require 'rest_client'
+
 require 'models/chef_configuration'
 require 'models/chef_executor'
 
@@ -12,10 +15,11 @@ conf.prepare
 # apply appstage configuration
 chef = ChefExecutor.new(conf.conf_template[:config_path], conf.chef_solo)
 
-if ENV.key? 'NODE'
-  chef.run( :data => ENV['NODE'] )
-else
-  puts "Environment variable NODE is not set"
+if ['NODE', 'AUTO_SCALING_SERVER', 'SERVICE_ID', 'VM_ID'].all? {|k| ENV.include?(k)}
+  raise RuntimeError, "Can't configure and converge environment. One of the values: NODE, AUTO_SCALING_SERVER, SERVICE_ID, VM_ID cannot be find in env: #{ENV}"
+  return
 end
 
+chef.run( :data => ENV['NODE'] )
+RestClient.post "#{ENV['AUTO_SCALING_SERVER']}/service/#{ENV['SERVICE_ID']}/container/#{ENV['VM_ID']}", "{}"
 
