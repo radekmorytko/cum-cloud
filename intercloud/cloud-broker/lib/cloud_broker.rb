50226a7a14596c4cf31b6be6924e0b4e2938ef74
Radoslaw Morytko <radoslaw.morytko@gmail.com>
2013-08-06 00:55:17 +0200

    [#26] Add stub of a cloud broker publisher and db handling

--- intercloud/cloud-broker/lib/cloud_broker.rb
+++ intercloud/cloud-broker/lib/cloud_broker.rb
@@ -4,16 +4,44 @@ require 'bundler/setup'
 module Intercloud
   class CloudBroker
 
-    def initialize(settings)
-
+    def initialize(options = {})
+      @database  = options[:database]
+      @publisher = options[:publisher]
     end
 
     def deploy(service_spec, client_endpoint)
-      12
+      id = save_deploy_request(service_spec, client_endpoint)
+      @publisher.publish(id, service_spec)
+      id
     end
 
     def valid?(service_spec)
       true
+    end
+
+    private
+
+    def save_deploy_request(service_spec, client_endpoint)
+      database_record = prepare_database_record(
+          :service_spec    => service_spec,
+          :client_endpoint => client_endpoint
+      )
+      @database.set(database_record[:id], database_record[:record])
+      database_record[:id]
+    end
+
+    def generate_id(seed)
+      Digest::SHA1.hexdigest(seed)
+    end
+
+    def prepare_database_record(options)
+      {
+          :id => generate_id(options[:client_endpoint] + options[:service_spec]),
+          :record => JSON.generate({
+            :client_endpoint => options[:client_endpoint],
+            :service_spec    => options[:service_spec]
+          })
+      }
     end
   end
 end
