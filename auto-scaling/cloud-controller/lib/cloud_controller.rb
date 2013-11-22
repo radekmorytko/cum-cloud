5778e61e6f9494c3ddb807f21662a6255bb6ca3a
Radoslaw Morytko <radoslaw.morytko@gmail.com>
2013-11-22 16:16:23 +0100

    Get rid of offer consumer

--- auto-scaling/cloud-controller/lib/cloud_controller.rb
+++ auto-scaling/cloud-controller/lib/cloud_controller.rb
@@ -1,14 +1,20 @@
 require 'rubygems'
-require 'common/config_utils'
+require 'common/configurable'
 require 'cloud_controller/publisher'
-require 'cloud_controller/offer_consumer'
 require 'logger'
 require 'amqp'
+require 'json'
+## TODO remove this once the offer mechanism is done
+require 'securerandom'
 
 module AutoScaling
   class CloudController
+    include Configurable
+
     @@logger = Logger.new(STDOUT)
     @@instance = nil
+
+    attr_reader :amqp_thread
 
     # Handle request passed from lower layer (service-controller)
     #
@@ -21,25 +27,49 @@ module AutoScaling
 
     def self.build
       raise 'There can be only one instance of CloudController' unless @@instance.nil?
-      Thread.start { CloudController.run }
       @@instance = CloudController.new
     end
 
-    def self.run
+
+    private
+    def initialize
+      @publisher   = Publisher.new
+      @amqp_thread = Thread.new { run }
+    end
+
+    def handle_offer_request(metadata, payload)
+      @@logger.info("Handling an offer request")
+      message = JSON.parse(payload)
+
+      mock_offer = {
+        :cost          => SecureRandom.random_number(100),
+        :controller_id => config['amqp']['controller_routing_key'],
+        :service_id    => message['service_id']
+      }.to_json
+
+      @publisher.publish(mock_offer, :routing_key => message['offers_routing_key'])
+      @@logger.info("Sent a mock offer: #{mock_offer}")
+
+    end
+
+    def handle_deploy_request(metadata, payload)
+      @@logger.info("Handling a deploy request")
+    end
+
+    def run
       EM.run do
 
-        config = ConfigUtils.load_config
         amqp_conf = config['amqp']
 
         AMQP.connect(:host => amqp_conf['host']) do |connection|
           @@logger.info('Connected to AMQP')
           channel = AMQP::Channel.new(connection)
 
-          offer_consumer = OfferConsumer.new(Publisher.new(channel.default_exchange))
+          @publisher.exchange = channel.default_exchange
 
           offers_exchange = channel.fanout(amqp_conf['offers_exchange_name'])
-          channel.queue('').bind(offers_exchange).subscribe(&offer_consumer.method(:handle_offer_request))
-          channel.queue(amqp_conf['controller_routing_key']).subscribe(&offer_consumer.method(:handle_deploy_request))
+          channel.queue('').bind(offers_exchange).subscribe(&method(:handle_offer_request))
+          channel.queue(amqp_conf['controller_routing_key']).subscribe(&method(:handle_deploy_request))
         end
 
       end
