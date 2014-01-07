715290d87c0ed7c7f7eb0f68abc9e857e8b463c2
Radoslaw Morytko <radoslaw.morytko@gmail.com>
2014-01-07 16:43:22 +0100

    Do not respond with an offer with already-deployed service

--- auto-scaling/cloud-controller/lib/cloud_controller/service_offer_preparer.rb
+++ auto-scaling/cloud-controller/lib/cloud_controller/service_offer_preparer.rb
@@ -3,23 +3,28 @@ require 'logger'
 require 'common/configurable'
 require 'cloud_controller/stack_offer_preparer'
 
-class ServiceOfferPreparer
-  include Configurable
+module AutoScaling
+  class ServiceOfferPreparer
+    include Configurable
 
-  @@logger       = Logger.new(STDOUT)
-  @@logger.level = Logger::DEBUG
+    @@logger       = Logger.new(STDOUT)
+    @@logger.level = Logger::DEBUG
 
 
-  def initialize(stack_offer_preparer)
-    @stack_offer_preparer = stack_offer_preparer
-  end
+    def initialize(stack_offer_preparer)
+      @stack_offer_preparer = stack_offer_preparer end
 
-  def prepare_offer(service_specification)
-    prepare_stacks_offers(service_specification['stacks'])
-  end
+    def prepare_offer(service_specification)
+      if not Service.get(service_specification['service_name'])
+        prepare_stacks_offers(service_specification['stacks'])
+      else
+        nil
+      end
+    end
 
-  private
-  def prepare_stacks_offers(stacks)
-    stacks.map { |stack| @stack_offer_preparer.prepare_offer(stack) }.select { |offer| not offer.nil? }
+    private
+    def prepare_stacks_offers(stacks)
+      stacks.map { |stack| @stack_offer_preparer.prepare_offer(stack) }.select { |offer| not offer.nil? }
+    end
   end
 end
